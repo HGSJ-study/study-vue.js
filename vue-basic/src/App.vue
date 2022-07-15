187acb428cd2ce5926b400ed5ac080e766a2555b
gyeol2678 <gyeol2678@gmail.com>
2022-07-16 00:58:54 +0900

    new: 'ConditionalRender' examples

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -2,19 +2,22 @@
   <DeclarativeRender />
   <InputHandling />
   <TwoWayBinding />
+  <ConditionalRender />
 </template>
 
 <script>
 import DeclarativeRender from './components/001_DeclarativeRender.vue'
 import InputHandling from './components/002_InputHandling.vue'
 import TwoWayBinding from './components/003_TwoWayBinding.vue'
+import ConditionalRender from './components/004_ConditionalRender.vue'
 
 export default {
   name: 'App',
   components: {
     DeclarativeRender,
     InputHandling,
-    TwoWayBinding
+    TwoWayBinding,
+    ConditionalRender
   }
 }
 </script>
---
+++ vue-basic/src/components/004_ConditionalRender.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>4. Conditional Rendering</h1>
+  <span v-if="seen">Condition: True!! Now you can see me</span>
+</template>
+
+<script>
+export default {
+  name: 'ConditionalRender',
+  data() {
+    return {
+      seen: true
+    }
+  }
+}
+</script>
