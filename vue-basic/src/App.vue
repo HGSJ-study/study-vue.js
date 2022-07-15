59a7e6b5aa8b685049f3c8306f9b64e6d04070f7
gyeol2678 <gyeol2678@gmail.com>
2022-07-15 23:37:34 +0900

    new: Create 'DeclarativeRender' component

---
+++ vue-basic/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <DeclarativeRender />
+</template>
+
+<script>
+import DeclarativeRender from './components/DeclarativeRender.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DeclarativeRender
+  }
+}
+</script>
---
+++ vue-basic/src/components/DeclarativeRender.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'DeclarativeRender',
+  data() {
+    return {
+      msg: 'Declarative Rendering!!'
+    }
+  }
+}
+</script>
